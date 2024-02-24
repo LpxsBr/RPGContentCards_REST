26254155c5cd5128dd71894aa079fcaf15d3301d
Castelo <i.castelobp@gmail.com>
2024-02-24 08:36:40 -0300

    Ajuste nos modelos de Spells e Itens

--- src/database/models/itemModel.js
+++ src/database/models/itemModel.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const Item = mongoose.model('item', {
-    name: String,
-    attunement: Boolean,
-    type: String,
-    rarity: String,
-    description: Array,  
-})
+const Item = mongoose.model("item", {
+  name: String,
+  attunement: Boolean,
+  type: String,
+  subtype: String,
+  rarity: String,
+  powerIdentity: Array,
+  description: Array,
+});
 
-export { Item }
+export { Item };
--- src/database/models/speellModel.js
+++ src/database/models/speellModel.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
-const Spell = mongoose.model('spell', {
-    name: String,
-    level: Number,
-    school: String,
-    description: Array,
-    castingTime: String,
-    range: String,
-    components: Array,
-    duration: String
+const Spell = mongoose.model("spell", {
+  name: String,
+  level: Number,
+  conjurer: Array,
+  school: String,
+  description: Array,
+  castingTime: String,
+  range: String,
+  damageResume: Array,
+  damageType: String,
+  components: Array,
+  duration: String,
+  source: String,
 });
 
 export { Spell };
