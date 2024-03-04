6e78594f280e76f3111c3c2b48287de4d5497bf7
lpxsbr <anselmolopes.an@gmail.com>
2024-03-04 13:16:55 -0300

    feat: add deletion of itens

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -2,7 +2,7 @@ import { Item } from "../database/models/itemModel.js"
 
 export default new class ItemController {
     async create(req, res) {
-      
+
         const itemBody = req.body
 
         try {
@@ -23,14 +23,33 @@ export default new class ItemController {
         }
     }
 
-    async getAll(req, res){
+    async getAll(req, res) {
         try {
-            
+
             const item = await Item.find({})
-            
+
             return res.status(201).json({
                 message: 'collection with all items returned',
                 data: item
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                message: 'item was not create',
+                error
+            })
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            const { id } = req.params;
+
+            await Item.deleteOne({ _id: id })
+
+            return res.status(200).json({
+                message: 'item deleted',
+                id
             })
 
         } catch (error) {
